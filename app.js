10ff189bf2f3e552e3b5653a91b01fe89ec82a0f
Furkan Portakal <furkan.portakal@demirorenteknoloji.com>
2023-03-06 15:32:02 +0300

    fix generator func for nested object

--- app.js
+++ app.js
@@ -29,43 +29,37 @@ function createJsonSchema(jsonData) {
 
   for (let key in jsonData) {
     if (Array.isArray(jsonData[key])) {
-      schema.properties[key] = {
-        type: "array",
-        items: {
-          type: "object",
-          properties: createJsonSchema(jsonData[key][0]).properties,
-        },
-      };
-
-      // Özel durum: Dizinin elemanları string ise "items" şemasını güncelle
-      if (jsonData[key].length > 0 && typeof jsonData[key][0] === "string") {
-        schema.properties[key].items = {
-          type: "string",
+      if (jsonData[key].length > 0 && typeof jsonData[key][0] === "object") {
+        schema.properties[key] = {
+          type: "array",
+          items: {
+            type: "object",
+            properties: createJsonSchema(jsonData[key][0]).properties,
+          },
+        };
+      } else {
+        schema.properties[key] = {
+          type: "array",
+          items: {
+            type: typeof jsonData[key][0],
+          },
         };
       }
     } else if (typeof jsonData[key] === "object") {
-      // Özel durum: null değeri için şema güncellemesi
-      if (jsonData[key] === null) {
-        schema.properties[key] = {
-          type: "object",
-          properties: createJsonSchema(jsonData[key]).properties,
-        };
-      }
-
       if (jsonData[key] === null) {
         schema.properties[key] = {
           type: "null",
+        };
+      } else {
+        schema.properties[key] = {
+          type: "object",
+          properties: createJsonSchema(jsonData[key]).properties,
         };
       }
     } else {
       schema.properties[key] = {
         type: typeof jsonData[key],
       };
-
-      // Özel durum: null değeri için şema güncellemesi
-      if (jsonData[key] === null) {
-        schema.properties[key].type = "null";
-      }
     }
   }
 
